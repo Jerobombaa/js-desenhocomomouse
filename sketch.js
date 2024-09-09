f1ae03ba63e736e9353743467c6c218016572d05
Jerobombaa <miguel.silva.lucas@escola.pr.gov.br>
2024-09-09 10:50:52 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(800, 800);
+    background("white");
+  }
+  
+  function draw() {
+     stroke("black");
+      fill("black");
+       if(mouseIsPressed){
+        rect(mouseX,mouseY,20,20); 
+      
+    }
+    
+    
+    
+  }
